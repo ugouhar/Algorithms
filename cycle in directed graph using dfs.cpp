e7ce38fca55b8182bb8a5cc47dde1288ae201ba2
gouhar1512 <ushran987@gmail.com>
2020-08-20 17:19:42 +0530

    cycle in directed and undirected graph

--- cycle in directed graph using dfs.cpp
+++ cycle in directed graph using dfs.cpp
@@ -1,8 +1,15 @@
 const ll N = 100;
 vll adj[N];
-ll par[N];
-bool vis[N];
+ll color[N];
 bool cycle = false;
+
+void init() {
+    for(ll i = 0 ; i < N ; i++) {
+        adj[i].clear();
+        color[i] = 0;
+    }
+    cycle = false;
+}
 
 void dfs(int s, int p) {
     color[s] = 1;
@@ -15,25 +22,4 @@ void dfs(int s, int p) {
         }
     }
     color[s] = 2;
-}
-
-void init() {
-    for(ll i = 0 ; i < N ; i++) {
-        adj[i].clear();
-        vis[i] = false;
-        par[i] = i;
-    }
-    cycle = false;
-}
-
-void detectCycle(ll s) {
-    vis[s] = true;
-    for(auto i : adj[s]) {
-        if(!vis[i]) {
-            par[i] = s;
-            detectCycle(i);
-        } else if(par[s] != i) {
-            cycle = true;
-        }
-    }
 }
