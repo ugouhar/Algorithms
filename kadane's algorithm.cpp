9f020c8981068e9c72e4fcdac8fc52880c061916
gouhar1512 <ushran987@gmail.com>
2020-08-23 02:04:53 +0530

    Update Kadane's algo

--- kadane's algorithm.cpp
+++ kadane's algorithm.cpp
@@ -1,30 +1,10 @@
-/*To find the largest sum in contiguous subarray*/
-#include<bits/stdc++.h>
-using namespace std;
-
-int kadane(int *a,int n)
-{
-    int currHigh=a[0],globHigh=a[0];
-    for(int i=1 ; i<n ; i++)
-    {
-        currHigh=max(a[i],currHigh+a[i]);
-        if(currHigh>globHigh)
-            globHigh=currHigh;
-    }
-    return globHigh;
-}
-int main()
-{
-    int t,n,i,sum;
-    cin>>t;
-    while(t--)
-    {
-        cin>>n;
-        int *a=new int[n];
-        for(i=0 ; i<n ; i++)
-            cin>>a[i];
-        sum=kadane(a,n);
-        cout<<sum<<endl;
-    }
-    return 0;
+int kadaneAlgo(int a[], int n) {
+  int maxSum = a[0], currSum = a[0];
+  for(int i=1 ; i<n ; i++){
+    currSum += a[i];
+    if(currSum < a[i])
+      currSum = a[i];
+    maxSum = max(maxSum, currSum);
+  }
+  return maxSum;
 }
